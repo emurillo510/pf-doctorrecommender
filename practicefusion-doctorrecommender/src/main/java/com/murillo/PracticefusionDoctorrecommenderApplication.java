95c073e60be71e163621c01e31daab288d3d0fca
Elton Murillo <emurillo510@gmail.com>
2016-10-03 07:31:09 -0700

    Initial Commit

---
+++ practicefusion-doctorrecommender/src/main/java/com/murillo/PracticefusionDoctorrecommenderApplication.java
@@ -0,0 +1,12 @@
+package com.murillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticefusionDoctorrecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticefusionDoctorrecommenderApplication.class, args);
+	}
+}
