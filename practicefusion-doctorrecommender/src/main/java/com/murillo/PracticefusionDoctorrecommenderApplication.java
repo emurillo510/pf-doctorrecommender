ded7668dc463b44a3e5fde6b3f0c1ebeb24d4440
Elton Murillo <emurillo510@gmail.com>
2016-10-04 07:36:27 -0700

    Adds logic for processing doctor data given a given Doctor

--- practicefusion-doctorrecommender/src/main/java/com/murillo/PracticefusionDoctorrecommenderApplication.java
+++ practicefusion-doctorrecommender/src/main/java/com/murillo/PracticefusionDoctorrecommenderApplication.java
@@ -1,12 +1,88 @@
 package com.murillo;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.murillo.models.Doctor;
+import com.murillo.repositories.DoctorRepository;
+import com.murillo.services.DoctorService;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.murillo.repositories")
+@EntityScan("com.murillo.models")
+@ComponentScan("com.murillo.services")
 public class PracticefusionDoctorrecommenderApplication {
+	
+	static Logger logger = Logger.getLogger(PracticefusionDoctorrecommenderApplication.class);
+	
+	@Autowired
+	DoctorService doctorService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticefusionDoctorrecommenderApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(DoctorRepository doctorRepo) {
+		
+		logger.info("Application Start ----------------------------------------------");
+
+		/*
+		 * private Long id;
+		private String firstName;
+		private String middleName;
+		private String lastName;
+		private String address1;
+		private String phoneNumber;
+		private String age;
+		private String sex;
+		private String schoolAttended;
+		private String academicDegree;
+		private String specialityArea;
+		private String availablityAfterHours;
+		private String personalityType;
+		private String patientsSaved;
+		private String patientsServed;
+		private String fatalities;
+		private String yearsOfExperience;
+		private String positiveReviews;
+		private String negativeReviews;
+		private String officeLocation;
+		 * */
+		Doctor elton = new Doctor();
+		elton.setAge("26");
+		elton.setSex("male");
+		elton.setSchoolAttended("San Jose State University");
+		elton.setAcademicDegree("BS");
+		elton.setSpecialityArea("Dermatologist");
+		elton.setAvailablityAfterHours("yes");
+		elton.setPersonalityType("fun");
+		elton.setPatientsServed("95");
+		elton.setPatientsSaved("101");
+		elton.setFatalities("0");
+		elton.setYearsOfExperience("3");
+		elton.setPositiveReviews("51");
+		elton.setNegativeReviews("1");
+		elton.setOfficeLocation("San Francisco");
+		
+		logger.info("Similar Doctor Service ----------------------------------------------");
+		for(Doctor d: doctorService.processDoctors(elton)) {
+			logger.info(d.getFirstName() + " " + d.getLastName());
+			
+		}
+		logger.info("Similar Doctor Service End----------------------------------------------");
+		
+		logger.info("Application End ----------------------------------------------");
+			
+		return null;
 	}
 }
--- practicefusion-doctorrecommender/src/main/java/com/murillo/services/DoctorService.java
+++ practicefusion-doctorrecommender/src/main/java/com/murillo/services/DoctorService.java
@@ -17,19 +17,234 @@ public class DoctorService {
 	public List<Doctor> processDoctors(Doctor d) {
 		
 		List<Doctor> processedDoctors = (List<Doctor>) doctorRepo.findAll();
-		
-		
+					
+		processedDoctors = similarAge(processedDoctors, d);
+		processedDoctors = similarSex(processedDoctors, d);
+		processedDoctors = similarSchools(processedDoctors, d);
+		processedDoctors = availablityAfterHours(processedDoctors, d);
+		processedDoctors = similarPersonality(processedDoctors, d);
+		processedDoctors = similarPatientsSaved(processedDoctors, d);
+		processedDoctors = similarPatientsServed(processedDoctors, d);
+		processedDoctors = similarFatalities(processedDoctors, d);
+		processedDoctors = similarYearsOfExperience(processedDoctors, d);
+		processedDoctors = similarNegativeReviews(processedDoctors, d);
+		processedDoctors = similarOfficeLocation(processedDoctors, d);
+			
 		return processedDoctors;
-		
 		
 	}
 	
-	/*  private Long id;
-		private String firstName;
-		private String middleName;
-		private String lastName;
-		private String address1;
-		private String phoneNumber;
+	public List<Doctor> similarAge(List<Doctor> doctors, Doctor d) {
+		
+		final int doctorAge = Integer.parseInt(d.getAge());
+		final int RANGE = 5;
+		final int MIN_AGE = doctorAge - RANGE;
+		final int MAX_AGE = doctorAge + RANGE;
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final int currentDoctorAge = Integer.parseInt(doctors.get(i).getAge());
+			if( !(MIN_AGE < currentDoctorAge) && !(currentDoctorAge < MAX_AGE)) {
+				doctors.remove(i);
+			}	
+		}
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarSex(List<Doctor> doctors, Doctor d) {
+		
+		final String doctorSex = d.getSex().toLowerCase().trim();
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			
+			final String currentDoctorSex = doctors.get(i).getSex().toLowerCase().trim();
+		
+			if(!doctorSex.equals(currentDoctorSex)) {
+				doctors.remove(i);
+			}
+		}
+		
+		return doctors;
+	
+		
+	}
+	
+	public List<Doctor> similarSchools(List<Doctor> doctors, Doctor d) {
+		
+		// if doctor academic program ranks within 10 schools
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarSpecialityArea(List<Doctor> doctors, int index, Doctor d) {
+		
+		final String doctorSpecialityArea = d.getSpecialityArea().toLowerCase().trim();
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final String currentDoctor = doctors.get(i).getSpecialityArea().toLowerCase().trim();
+			
+			if(!doctorSpecialityArea.equals(currentDoctor)) {
+				doctors.remove(i);
+			}
+		}
+		
+		
+		return doctors;
+	}
+	
+	public List<Doctor> availablityAfterHours(List<Doctor> doctors, Doctor d) {
+		
+		final String doctorAfterHours = d.getAvailablityAfterHours().toLowerCase().trim();
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final String currentDoctorAfterHours = doctors.get(i).getAvailablityAfterHours().toLowerCase().trim();
+			if(!doctorAfterHours.equals(currentDoctorAfterHours)) {
+				doctors.remove(i);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarPersonality(List<Doctor> doctors, Doctor d) {
+		
+		final String doctorPersonality = d.getPersonalityType().toLowerCase().trim();
+		for(int i = 0; i < doctors.size(); i++) {
+			final String currentDoctorsPersonality = doctors.get(i).getPersonalityType().toLowerCase().trim();
+			
+			if(!doctorPersonality.contains(currentDoctorsPersonality)) {
+				doctors.remove(i);
+			}
+		}
+		
+		
+		
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarPatientsSaved(List<Doctor> doctors, Doctor d) {
+		
+		
+		final double doctorPatientsSaved = Integer.parseInt(d.getPatientsSaved());
+		final double RANGE = doctorPatientsSaved * 0.10;
+		final double MIN_AGE = doctorPatientsSaved - RANGE;
+		final double MAX_AGE = doctorPatientsSaved + RANGE;
+		
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final double currentDoctorPatientsSaved = Integer.parseInt(doctors.get(i).getPatientsSaved());
+			
+			if( !(MIN_AGE < currentDoctorPatientsSaved) && !(currentDoctorPatientsSaved < MAX_AGE)) {
+				doctors.remove(i);
+			}
+		}
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarPatientsServed(List<Doctor> doctors, Doctor d) {
+		
+		final double doctorPatientsServed = Integer.parseInt(d.getPatientsServed());
+		final double RANGE = doctorPatientsServed * 0.10;
+		final double MIN_AGE = doctorPatientsServed - RANGE;
+		final double MAX_AGE = doctorPatientsServed + RANGE;
+		
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final double currentDoctorPatientsServed = Integer.parseInt(doctors.get(i).getPatientsServed());
+			if( !(MIN_AGE < currentDoctorPatientsServed) && !(currentDoctorPatientsServed < MAX_AGE)) {
+				doctors.remove(i);
+			}
+		}
+
+		return doctors;
+	}
+	
+	public List<Doctor> similarFatalities(List<Doctor> doctors, Doctor d) {
+		
+		final double doctorFatalities = Integer.parseInt(d.getFatalities());
+		final double RANGE = doctorFatalities * 0.10;
+		final double MIN_AGE = doctorFatalities - RANGE;
+		final double MAX_AGE = doctorFatalities + RANGE;
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final double currentDoctorFatalities = Integer.parseInt(doctors.get(i).getFatalities());
+			
+			if( !(MIN_AGE < currentDoctorFatalities) && !(currentDoctorFatalities < MAX_AGE)) {
+				doctors.remove(i);
+			}
+		}
+		return doctors;
+	}
+	
+	public List<Doctor> similarYearsOfExperience(List<Doctor> doctors, Doctor d) {
+		
+		final int doctorYearsOfExperience = Integer.parseInt(d.getYearsOfExperience());
+		final int RANGE = 3;
+		final int MIN_AGE = doctorYearsOfExperience - RANGE;
+		final int MAX_AGE = doctorYearsOfExperience + RANGE;
+		
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final int currentYearsOfExperience = Integer.parseInt(doctors.get(i).getYearsOfExperience());
+			
+			if( !(MIN_AGE < currentYearsOfExperience) && !(currentYearsOfExperience < MAX_AGE)) {
+				doctors.remove(i);
+			}
+		}
+		
+		return doctors;
+	}
+	
+	public List<Doctor> similarNegativeReviews(List<Doctor> doctors, Doctor d) {
+		
+		final int doctorNegativeReviews = Integer.parseInt(d.getNegativeReviews());
+		final int RANGE = 15;
+		final int MIN_AGE = doctorNegativeReviews - RANGE;
+		final int MAX_AGE = doctorNegativeReviews + RANGE;
+		
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final int currentNegativeReviews = Integer.parseInt(doctors.get(i).getNegativeReviews());
+			
+			if( !(MIN_AGE < currentNegativeReviews) && !(currentNegativeReviews < MAX_AGE)) {
+				doctors.remove(i);
+			}
+		}
+		
+
+		return doctors;
+	}
+	
+	public List<Doctor> similarOfficeLocation(List<Doctor> doctors, Doctor d) {
+		
+		
+		final String doctorSpecialityArea = d.getOfficeLocation().toLowerCase().trim();
+		
+		for(int i = 0; i < doctors.size(); i++) {
+			final String currentDoctor = doctors.get(i).getOfficeLocation().toLowerCase().trim();
+			
+			if(!doctorSpecialityArea.contains(currentDoctor)) {
+				doctors.remove(i);
+			}
+		}
+			
+
+		return doctors;
+	}
+	
+	
+	
+	
+	
+	
+	/*
 		private String age;
 		private String sex;
 		private String schoolAttended;
